fe5617337d6224888427dd6cfc1b084e3d802ec7
Jean-Francois Theroux <failshell@gmail.com>
2013-09-30 20:45:32 -0400

    Update metadata.rb

--- metadata.rb
+++ metadata.rb
@@ -1,6 +1,6 @@
 name             "tinyproxy"
 maintainer       "Jean-Francois Theroux"
-maintainer_email "failshell@gmail.com"
+maintainer_email "me@failshell.io"
 license          "Apache2"
 description      "Installs/Configures tinyproxy"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
